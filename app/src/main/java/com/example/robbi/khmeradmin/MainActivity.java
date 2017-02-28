f6c98e5f0419453eb92e835927b4011e298678b1
rob <robbiestells@hotmail.com>
2017-02-28 13:42:02 -0500

    update: changed code to run from async task

--- app/src/main/java/com/example/robbi/khmeradmin/MainActivity.java
+++ app/src/main/java/com/example/robbi/khmeradmin/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,11 +13,9 @@ import android.widget.ListView;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.microsoft.windowsazure.mobileservices.*;
-import com.microsoft.windowsazure.mobileservices.http.OkHttpClientFactory;
 import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
 import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
 import com.microsoft.windowsazure.mobileservices.table.TableJsonQueryCallback;
-import com.squareup.okhttp.OkHttpClient;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -36,7 +33,6 @@ import Adapters.ResponseAdapter;
 import models.Response;
 
 import static android.view.View.GONE;
-import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.val;
 
 public class MainActivity extends AppCompatActivity {
     MobileServiceClient mClient;
@@ -51,86 +47,63 @@ public class MainActivity extends AppCompatActivity {
         context = this;
 
         try {
-            // Mobile Service URL and key
             mClient = new MobileServiceClient(
-                    "https://khmer.azurewebsites.net",
+                    "http://khmer.azurewebsites.net/",
                     this);
 
-            // Extend timeout from default of 10s to 20s
-            mClient.setAndroidHttpClientFactory(new OkHttpClientFactory() {
-                @Override
-                public OkHttpClient createOkHttpClient() {
-                    OkHttpClient client = new OkHttpClient();
-                    client.setReadTimeout(20, TimeUnit.SECONDS);
-                    client.setWriteTimeout(20, TimeUnit.SECONDS);
-                    return client;
-                }
-            });
 
             mResponseTable = mClient.getTable(Response.class);
 
             mAdapter = new ResponseAdapter(this, R.layout.response_row);
+
             ListView responseListView = (ListView) findViewById(R.id.responseList);
             responseListView.setAdapter(mAdapter);
 
-            refreshItemsFromTable();
 
-            //  ResponseAsync async = new ResponseAsync();
-           //   async.execute();
+             ResponseAsync async = new ResponseAsync();
+              async.execute();
         } catch (MalformedURLException e) {
-            Log.v("Getting Table", "There was an error creating the Mobile Service. Verify the URL");
+            e.printStackTrace();
+        }
+    }
 
-        }catch (Exception e){
-            Log.v("Getting Table", "Error");
+    private class ResponseAsync extends AsyncTask<Void, Void, Void> {
+        List<Response> results = null;
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(context);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialog.show();
         }
 
-    }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
 
-    private void refreshItemsFromTable() {
-
-        // Get the items that weren't marked as completed and add them in the
-        // adapter
-
-        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
-            @Override
-            protected Void doInBackground(Void... params) {
-
-                try {
-                    final List<Response> results = refreshItemsFromMobileServiceTable();
-
-                    //Offline Sync
-                    //final List<ToDoItem> results = refreshItemsFromMobileServiceTableSyncTable();
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            mAdapter.clear();
-
-                            for (Response item : results) {
-                                mAdapter.add(item);
-                            }
-                        }
-                    });
-                } catch (final Exception e){
-                    Log.v("Async Task", "Error");
-                }
-
-                return null;
+                results = mResponseTable.where().execute().get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
             }
-        };
 
-        runAsyncTask(task);
-    }
+            return null;
+        }
 
-    private List<Response> refreshItemsFromMobileServiceTable() throws ExecutionException, InterruptedException {
-        return mResponseTable.where().execute().get();
-    }
-
-    private AsyncTask<Void, Void, Void> runAsyncTask(AsyncTask<Void, Void, Void> task) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            return task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-        } else {
-            return task.execute();
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if (results != null) {
+                mAdapter.clear();
+                Collections.reverse(results);
+                for (Response item : results) {
+                    mAdapter.add(item);
+                }
+            }
+            progressDialog.dismiss();
         }
     }
 }
